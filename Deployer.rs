4f29c330d1dd329962921f1708e5b33a0b3394f1
Wade <94668210+thegoodwei@users.noreply.github.com>
2022-12-28 15:07:26 -0600

    Update Deployer.rs

--- Deployer.rs
+++ Deployer.rs
@@ -1,6 +1,14 @@
-/*
-This deployer can be compiled to WASM using the `wasm-pack` tool and deployed to a web server or a decentralized storage platform, such as IPFS or Arweave. The deployer can then be invoked from a JavaScript application or a web page, passing the required arguments and receiving the transaction hashes of the
+/*This deployer.rs file is a Rust program that can be used to deploy an ERC1155 smart contract that holds the private encryption key 
+for an encrypted video file stored on Arweave. The deployer reads an unencrypted video file and a thumbnail image file, encrypts the 
+video file using Widevine, stores the encrypted video on Arweave, and creates a specified number of tokens with the 
+private encryption key and transaction ID of the encrypted video stored on the smart contract. 
+
+The deployer requires several command line arguments, including the number of tokens to create, the paths to the thumbnail and
+video files, the percentage of the token sale price to be paid as a royalty, the Ethereum address of the royalty beneficiary, 
+and the URLs of the Ethereum JSON-RPC endpoint, Ethereum wallet, and Arweave node, as well as the base64-encoded Arweave key file.
+Upon successful execution, the deployer returns a list of transaction hashes for the token creation transactions
 */
+
 
 // Import the required crates and libraries
 extern crate web3;
@@ -13,6 +21,7 @@ extern crate wasm_bindgen;
 extern crate wasm_bindgen_futures;
 extern crate serde_json;
 extern crate futures;
+extern crate rust_widevine;
 
 use std::env;
 use std::fs::File;
@@ -28,6 +37,7 @@ use image::{ImageFormat, ImageRgba8};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
 use serde_json::Value;
+use rust_widevine::{KeyType, LicenseRequest, Widevine};
 
 #[wasm_bindgen]
 // Define the deployer function
@@ -35,6 +45,8 @@ pub fn deploy(
     x_qty: i32,
     thumbnail_b64: &str,
     video_b64: &str,
+    royalty_percentage: i32,
+    royalty_beneficiary: &str,
     rpc_url: &str,
     wallet_url: &str,
     arweave_url: &str,
@@ -44,44 +56,37 @@ pub fn deploy(
     let x_qty = U256::from(x_qty as u64);
     let thumbnail_b64 = thumbnail_b64.to_string();
     let video_b64 = video_b64.to_string();
+    let royalty_percentage = U256::from(royalty_percentage as u64);
+    let royalty_beneficiary = Address::from_str(royalty_beneficiary).unwrap();
     let rpc_url = rpc_url.to_string();
-    let wallet_url = wallet_url.to_string();
-    let arweave_url = arweave_url.to_string();
-    let arweave_key = arweave_key.to_string();
+let wallet_url = wallet_url.to_string();
+let arweave_url = arweave_url.to_string();
+let arweave_key = arweave_key.to_string();
 
-    // Create a web3 instance and connect to the Ethereum network
-    let (_eloop, transport) = web3::transports::Http::new(&rpc_url).unwrap();
-    let web3 = web3::Web3::new(transport);
-
-    // Create a web3 contract instance and deploy the smart contract
-    let contract = EthContract::from_json(
-        web3.eth(),
-        Address::from_str("0x0000000000000000000000000000000000000000").unwrap(),
-        include_bytes!("../res/EncryptedVideoToken.json"),
-    )
-    .unwrap();
-    let options = Options::with(|opt| {
-        opt.gas = Some(3_000_000.into());
-        opt.value = Some(0.into());
-    });
-    let deploy = contract.deploy(options, thumbnail_b64, 10.into(), b"encrypted-video-token".to_vec());
-let address = deploy.send().wait().unwrap();
-let contract = Contract::from_ethcontract(web3.eth(), address, include_bytes!("../res/EncryptedVideoToken.json")).unwrap();
-
-
-// Read the encrypted video file and store it on Arweave
+// Read the unencrypted video file
 let mut video_file = File::open(&video_b64).unwrap();
 let mut video_bytes = Vec::new();
 video_file.read_to_end(&mut video_bytes).unwrap();
-let video_b64 = encode(&video_bytes);
-let transaction = Transaction::new(arweave_key.as_bytes());
-transaction.add_tag("Content-Type", "video/mp4");
-transaction.add_tag("Encryption", "Widevine");
-transaction.add_tag("Content-Length", &video_bytes.len().to_string());
-transaction.add_tag("Access-Key", &address.to_string());
-transaction.add_tag("Filename", "encrypted-video.mp4");
-transaction.set_data(video_b64.as_bytes());
-let transaction_id = transaction.send(arweave_url.as_str()).unwrap();
+
+// Encrypt the video file using Widevine
+let widevine = Widevine::new("/path/to/widevine/keys");
+let key_type = KeyType::ContentId;
+let request = LicenseRequest::new(
+    &video_bytes,
+    "video/mp4",
+    key_type,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+);
+let response = widevine.request_license(request).unwrap();
+let encrypted_video_bytes = response.ciphertext.unwrap();
+let key_id = hex::encode(response.key_id.unwrap());
 
 // Read the thumbnail image file and encode it as base64
 let mut thumbnail_file = File::open(&thumbnail_b64).unwrap();
@@ -89,10 +94,56 @@ let mut thumbnail_bytes = Vec::new();
 thumbnail_file.read_to_end(&mut thumbnail_bytes).unwrap();
 let thumbnail_b64 = encode(&thumbnail_bytes);
 
+// Store the encrypted video on Arweave
+let video_b64 = encode(&encrypted_video_bytes);
+let transaction = Transaction::new(arweave_key.as_bytes());
+transaction.add_tag("Content-Type", "video/mp4");
+transaction.add_tag("Encryption", "Widevine");
+transaction.add_tag("Content-Length", &encrypted_video_bytes.len().to_string());
+transaction.add_tag("Key-Id", &key_id);
+transaction.add_tag("Filename", "encrypted-video.mp4");
+transaction.set_data(video_b64.as_bytes());
+let transaction_id = transaction.send(arweave_url.as_str()).unwrap();
+
+// Create a web3 instance and connect to the Ethereum network
+let (_eloop, transport) = web3::transports::Http::new(&rpc_url).unwrap();
+let web3 = web3::Web3::new(transport);
+
+// Create a web3 contract instance and deploy the smart contract
+let contract = EthContract::from_json(
+    web3.eth(),
+    Address::from_str("0x0000000000000000000000000000000000000000").unwrap(),
+    include_bytes!("../res/EncryptedVideoToken.json"),
+).unwrap();
+let options = Options::with(|opt| {
+opt.gas = Some(3_000_000.into());
+opt.value = Some(0.into());
+});
+let deploy = contract.deploy(
+options,
+thumbnail_b64,
+royalty_percentage,
+royalty_beneficiary,
+key_id,
+b"encrypted-video-token".to_vec(),
+);
+let address = deploy.send().wait().unwrap();
+let contract = Contract::from_ethcontract(
+web3.eth(),
+address,
+include_bytes!("../res/EncryptedVideoToken.json"),
+)
+.unwrap();
+
 // Create the specified number of tokens using the smart contract
 let mut promises = Vec::new();
 for i in 0..x_qty {
-    let call = contract.call("create", (i, thumbnail_b64.clone(), transaction_id.clone()), options, wallet_url.as_str());
+    let call = contract.call(
+        "create",
+        (i, thumbnail_b64.clone(), transaction_id.clone()),
+        options,
+        wallet_url.as_str(),
+    );
     let promise = future_to_promise(call.send().then(move |result| {
         let result = result.unwrap();
         let receipt = result.wait().unwrap();
@@ -106,4 +157,5 @@ for i in 0..x_qty {
 future_to_promise(futures::future::join_all(promises).then(|results| {
     JsValue::from_serde(&results).unwrap()
 }))
-
+}
+    
